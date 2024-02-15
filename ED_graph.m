251ceab3ca1619ccedbfdce7af2a2549a29f0726
Sohail Dasgupta <sohaildasgupta@gmail.com>
2024-02-15 13:57:48 -0600

    more junk files removed

--- ED_graph.m
+++
@@ -1,4 +0,0 @@
-[g,coefficients] = NLCE_load(5);
-t=vpa(1); u=vpa(15.3); m=2; n=-1; dno=-1; mu=-u*(m-1)/2;
-itemlist={'density','doubleoccupancy','fileio'};
-[spectra, nsigmapermute, testn, nimatrix, domatrix] = ED_solver(g{1}, t, u, mu, n, m, dno,itemlist{:});
--- midpt_diff.m
+++
@@ -1,9 +0,0 @@
-function array = midpt_diff(x,y)
-a = [];
-for i=2:numel(y)-1
-    a(end+1) = (x(i)-x(i+1))/((x(i-1)-x(i))*(x(i-1)-x(i+1)))*y(i-1)...
-    +(2*x(i)-x(i-1)-x(i+1))/((x(i)-x(i-1))*(x(i)-x(i+1)))*y(i)...
-    +(x(i)-x(i-1))/((x(i+1)-x(i-1))*(x(i+1)-x(i)))*y(i+1);
-end
-array = a;
-end
